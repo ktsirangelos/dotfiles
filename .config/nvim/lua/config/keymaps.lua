a697dbc630c5270bcb00089d4182a9f0e2c29eb5
Konstantinos Tsirangelos <ktsirangelos@skroutz.gr>
2024-07-14 19:41:21 +0300

    Use stow for managing dotfiles

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
+
+-- select all
+keymap("n", "<leader>sA", "ggVG<CR>", { desc = "[S]elect [A]ll" })
+
+-- replace all
+keymap("n", "<leader>rA", ":%s/", { desc = "[R]eplace [A]ll" })
+
+-- move 1/2 page up/down and center the cursor on the screen
+keymap("n", "<C-d>", "<C-d>zz", default_opts)
+keymap("n", "<C-u>", "<C-u>zz", default_opts)
---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "mini.indentscope", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
