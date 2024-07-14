052d0bbf2b7eebbf9f6b17b2d33ca209a888a121
Konstantinos Tsirangelos <ktsirangelos@skroutz.gr>
2024-07-14 20:43:34 +0300

    Disable noice.nvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "mini.indentscope", enabled = false },
   { "lukas-reineke/headlines.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
