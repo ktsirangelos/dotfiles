5af17332823b2a057063f30b85992c1a98d66c75
Konstantinos Tsirangelos <ktsirangelos@skroutz.gr>
2024-09-14 19:09:15 +0300

    Update config

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,7 @@ return {
   { "mini.indentscope", enabled = false },
   { "lukas-reineke/headlines.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
 }
